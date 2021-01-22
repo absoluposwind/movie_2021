75ea737e668b8e346852f8551e40ee14f12d5dd7
absoluposwind <46222263+absoluposwind@users.noreply.github.com>
2021-01-22 17:28:47 +0900

    https://github.com/absoluposwind/movie_2021

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+
+function Food({name, picture, rating}){
+  return <div>
+  <h2>I like {name}</h2>
+  <h4>{rating}/5.0</h4>
+  <img src={picture} alt={name}/>
+  </div>
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+  };
+
+const foodIlike =[
+  {
+    id:1,
+    name: "kimchi",
+    image:
+    "https://www.maangchi.com/wp-content/uploads/2019/11/vegankimchi-insta.jpg",
+    rating: 5.0
+  },
+  {
+    id:2,
+    name: "ramen",
+    image:
+    "https://peasandcrayons.com/wp-content/uploads/2020/04/spicy-vegetarian-ramen-recipe-3.jpg",
+    rating: 4.8
+  },
+  {
+    id:3,
+    name: "kimbap",
+    image:
+    "https://www.seriouseats.com/2020/01/20200122-gimbap-vicky-wasik-24-1500x1125.jpg",
+    rating: 4.2
+  },
+  {
+    id:4,
+    name: "bulgogi",
+    image:"https://assets.bonappetit.com/photos/57acd741f1c801a1038bc801/8:5/w_4839,h_3024,c_limit/basic-bulgogi.jpg",
+    rating: 4.7
+  }
+];
+
+
+  
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div>
+      {foodIlike.map(dish =>(
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+      ))}
+    </div>;
 }
 
 export default App;
